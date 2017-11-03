87b019570d227c672bbe05c6acdb9e764cb8ba6f
up769598 <up769598@myport.ac.uk>
2017-11-03 23:29:59 +0000

    Test Statements Working
    Got the test statements working with some test data in the main

--- src/longpipe/LongPipe.java
+++ src/longpipe/LongPipe.java
@@ -18,91 +18,12 @@ public class LongPipe {
 
         PipeMain testpipe = new PipeMain(1, 1);
         testpipe.DisplayInfo();
-        TestPipeValid(true, true , 2 , 5);
+        Test test = new Test();
+        test.TestPipeValid(true, true , 2 , 5);
+        test.TestPipeValid(true, false , 2 , 5);
 
     }
 
-    public void TestPipeValid(boolean outerReinforcement, boolean innerInsulation, int colour, int plasticGrade) {
-        if (outerReinforcement) {
-            if (ValidatePipe5(innerInsulation, colour, plasticGrade)) {
-                System.out.println("Valid pipe type 5");
-            } else {
-                System.out.println("Invalid Pipe");
-                return;
-            }
-        } else if (innerInsulation) {
-            if (ValidatePipe4(colour, plasticGrade)) {
-                System.out.println("Valid pipe type 4");
-            } else {
-                System.out.println("Invalid Pipe");
-                return;
-
-            }
-
-        } else if (colour == 2) {
-            if (ValidatePipe3(plasticGrade)) {
-                System.out.println("Valid pipe type 3");
-            } else {
-                System.out.println("Invalid Pipe");
-                return;
-
-            }
-        } else if (colour == 1) {
-            if (ValidatePipe2(plasticGrade)) {
-                System.out.println("Valid pipe type 2");
-            } else {
-                System.out.println("Invalid Pipe");
-                return;
-
-            }
-        }else if (colour == 0) {
-            if (ValidatePipe1(plasticGrade)) {
-                System.out.println("Valid pipe type 1");
-            } else {
-                System.out.println("Invalid Pipe");
-                return;
-
-            }
-    }
-    
-
-    public boolean ValidatePipe5(boolean innerInsulation, int colour, int plasticGrade) {
-        if (plasticGrade >= 3 && plasticGrade <= 5 && colour == 2 && innerInsulation == true) {
-            return true;
-        } else {
-            return false;
-        }
-
-    }
-
-    public boolean ValidatePipe4(int colour, int plasticGrade) {
-        if (plasticGrade >= 2 && plasticGrade <= 5 && colour == 2) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public boolean ValidatePipe3(int plasticGrade) {
-        if (plasticGrade >= 2 && plasticGrade <= 5) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public boolean ValidatePipe2(int plasticGrade) {
-        if (plasticGrade >= 2 && plasticGrade <= 4) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public boolean ValidatePipe1(int plasticGrade) {
-        if (plasticGrade >= 1 && plasticGrade <= 3) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+   
 
 }
