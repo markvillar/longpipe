863c61d056ecfda53e7051b564aa1abc8263efa5
UP773229 <up773229@myport.ac.uk>
2017-11-05 02:38:51 +0000

    misspelt diameter

--- src/longpipe/Pipe4.java
+++ src/longpipe/Pipe4.java
@@ -15,8 +15,8 @@ public class Pipe4 extends PipeMain {
 
     }
 
-    public Pipe4(double p_length, double p_outerDiamater, boolean chemResistance) {
-        super(p_length, p_outerDiamater, chemResistance);
+    public Pipe4(double p_length, double p_outerDiameter, boolean chemResistance) {
+        super(p_length, p_outerDiameter, chemResistance);
         colour = 2;
     }
 
--- src/longpipe/PipeMain.java
+++ src/longpipe/PipeMain.java
@@ -13,7 +13,7 @@ public class PipeMain {
 
     protected double lenght;
     protected int plasticGrade;
-    protected double outerDiamater;
+    protected double outerDiameter;
     protected double plasticVolume;
     protected boolean chemicalResistance;
     protected int colour;
@@ -22,23 +22,23 @@ public class PipeMain {
 
     }
 
-    public PipeMain(double p_length, double p_outerDiamater, boolean chemResistance) {
+    public PipeMain(double p_length, double p_outerDiameter, boolean chemResistance) {
         lenght = p_length;
-        outerDiamater = p_outerDiamater;
-        plasticVolume = WorkOutVolume(outerDiamater, lenght);
+        outerDiameter = p_outerDiameter;
+        plasticVolume = WorkOutVolume(outerDiameter, lenght);
         chemicalResistance = chemResistance;
 
     }
 
-    protected double WorkOutVolume(double outerDiamater, double length) {
-        double innerDiamater = outerDiamater * 0.9;
+    protected double WorkOutVolume(double outerDiameter, double length) {
+        double innerDiamater = outerDiameter * 0.9;
         double volume;
-        volume = AreaOfCylnder(outerDiamater, lenght) - AreaOfCylnder(innerDiamater, lenght);
+        volume = AreaOfCylnder(outerDiameter, lenght) - AreaOfCylnder(innerDiamater, lenght);
         return volume;
     }
 
-    protected double AreaOfCylnder(double diamater, double length) {
-        return Math.sqrt((diamater / 2) * Math.PI) * length;
+    protected double AreaOfCylnder(double diameter, double length) {
+        return Math.sqrt((diameter / 2) * Math.PI) * length;
     }
 
     protected double InchToMeters(double inch) {
@@ -62,7 +62,7 @@ public class PipeMain {
     }
 
     public void DisplayInfo() {
-        System.out.println(lenght + " Meters : " + outerDiamater + " Inch : " + plasticVolume + " Inch^3 /n" + colour);
+        System.out.println(lenght + " Meters : " + outerDiameter + " Inch : " + plasticVolume + " Inch^3 /n" + colour);
     }
 
     protected double ChemicalPrice() {
