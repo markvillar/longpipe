69fb93d091ffd012d175329d4c189a31ae0fa75e
UP773229 <up773229@myport.ac.uk>
2017-11-07 18:15:13 +0000

    Added remove all button

--- src/longpipe/OrderForm.java
+++ src/longpipe/OrderForm.java
@@ -36,6 +36,7 @@ public class OrderForm extends javax.swing.JFrame {
         tfOutputCost = new javax.swing.JTextField();
         btnRemovePipe = new javax.swing.JButton();
         btnAddPipe = new javax.swing.JButton();
+        btnRemoveAll = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -66,6 +67,13 @@ public class OrderForm extends javax.swing.JFrame {
             }
         });
 
+        btnRemoveAll.setText("Remove All");
+        btnRemoveAll.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRemoveAllActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -78,7 +86,9 @@ public class OrderForm extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(tfOutputCost))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(btnRemovePipe)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(btnRemoveAll, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnRemovePipe, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 131, Short.MAX_VALUE)
                         .addComponent(btnAddPipe)))
                 .addContainerGap())
@@ -94,7 +104,9 @@ public class OrderForm extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnRemovePipe)
                     .addComponent(btnAddPipe))
-                .addContainerGap(17, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnRemoveAll)
+                .addContainerGap())
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -141,6 +153,12 @@ public class OrderForm extends javax.swing.JFrame {
         printPipeOutput();
     }//GEN-LAST:event_btnAddPipeActionPerformed
 
+    private void btnRemoveAllActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveAllActionPerformed
+        //Clear all added pipes
+        PipeOrder.clear();
+        printPipeOutput();
+    }//GEN-LAST:event_btnRemoveAllActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -178,6 +196,7 @@ public class OrderForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAddPipe;
+    private javax.swing.JButton btnRemoveAll;
     private javax.swing.JButton btnRemovePipe;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
