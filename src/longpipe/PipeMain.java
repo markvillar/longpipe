df8aa4b3c7bd33d541aeeb9f134ceed528b6a6af
up769598 <UP769598@uni.ds.port.ac.uk>
2017-11-10 12:51:43 +0000

    Added java doc to pipe main

--- src/longpipe/PipeMain.java
+++ src/longpipe/PipeMain.java
@@ -22,6 +22,14 @@ public class PipeMain {
 
     }
 
+    /**
+     * Main construtors for the defult Pipe
+     * @param p_length length specified by the user 
+     * @param p_outerDiameter width of the pipe specified by the user 
+     * @param plasgrd plastic grade specified by the user 
+     * @param chemResistance if the pipe is chemical resistant then the value will be true
+     * 
+     */
     public PipeMain(double p_length, double p_outerDiameter, int plasgrd, boolean chemResistance) {
         length = p_length;
         outerDiameter = p_outerDiameter;
@@ -30,6 +38,12 @@ public class PipeMain {
         plasticGrade = plasgrd;
     }
 
+    /**
+     * Used to work out the plastic Volume of the pipe by subtracting the inner diamiter cylinder by the outer diamiter cylinder
+     * @param outerDiameter the width of the pipe passed through by the constructor
+     * @param length the length of the pipe passed through by the constructor
+     * @return the plastic volume of the pipe 
+     */
     protected double WorkOutVolume(double outerDiameter, double length) {
         double innerDiamater = outerDiameter * 0.9;
         double volume;
@@ -37,45 +51,94 @@ public class PipeMain {
         return volume;
     }
 
+    /**
+     * Calculates the volume of a cylinder 
+     * @param diameter Diameter of the cylinder
+     * @param length Length of the cylinder
+     * @return returns the volume of the cylinder
+     */
     protected double VolumeOfCylinder(double diameter, double length) {
         return Math.pow((diameter / 2) * Math.PI, 2) * length;
     }
 
+    /**
+     * Converts inches to meters
+     * @param inch the length in inches to be converted into inch
+     * @return length in meters
+     */
     protected double InchToMeters(double inch) {
         return inch * 0.0254;
     }
 
+    /**
+     * Converts Meters To Inch
+     * @param meters the meters value to be converted to inch
+     * @return the converted meters value
+     */
     protected double MetersToInch(double meters) {
         return meters * 39.37;
     }
 
+    /**
+     * gets the value of chemical resistance
+     * @return chemical resistance
+     */
     public boolean GetChemicalResistance() {
         return chemicalResistance;
     }
 
+    /**
+     *  gets the value of plastic grade 
+     * @return plastic grade 
+     */
     public int GetPlasticGrade() {
         return plasticGrade;
     }
 
+    /**
+     *used to set the colour 
+     * @param col the color to be set to
+     */
     public void setColour(int col) {
         colour = col;
     }
 
+    /**
+     * gets the value of plastic Volume 
+     * @return plastic Volume 
+     */
     public double getPlasticVolume() {
         return plasticVolume;
     }
 
+    /**
+     * gets the value of plastic grade 
+     * @return Width of the pipe 
+     */
     public double getDiameter() {
         return outerDiameter;
     }
+
+    /**
+     * gets the value of length of the pipe
+     * @return length of pipe 
+     */
     public double getLength(){
         return length;
     }
 
+    /**
+     * used to return a string with the information of a pipe  
+     * @return formated string
+     */
     public String DisplayInfo() {
         return (" Meters : " + this.getLength() + " Inch : " + String.format("%.2f", this.getDiameter()) + " Price: £" + String.format("%.2f", this.Price()));
     }
 
+    /**
+     *
+     * @return
+     */
     protected double ChemicalPrice() {
         if (this.GetChemicalResistance()) {
             return 1.14;
