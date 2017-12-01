e60b6ceb283d5de6bfbba45464fc157349a3fb08
UP773229 <UP773229@myport.ac.uk>
2017-12-01 10:34:41 +0000

    Revert "direct call"
    This reverts commit 1322411163dc54696ea5039f45af16549d3f4937.

--- src/longpipe/Pipe.java
+++ src/longpipe/Pipe.java
@@ -141,7 +141,7 @@ public abstract class Pipe {
      * @return price increase for chemical resistance
      */
     protected double getChemicalPrice() {
-        if (getChemicalResistance()) {
+        if (this.getChemicalResistance()) {
             return 1.14;
         } else {
             return 1;
